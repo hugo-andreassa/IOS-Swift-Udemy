1082c305dadece833eabe6cbb481bd2e594f6427
Hugo Andreassa <hugo.andreassa@gmail.com>
2021-08-31 14:32:17 -0300

    Xylophone

--- 4 - xylophone/Xylophone/ViewController.swift
+++ 4 - xylophone/Xylophone/ViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.isOpaque = true
+        playSound(sender.currentTitle!)
+        
+        // Faz a trend pausar por 0.2 segundos
+        usleep(200000)
+        
+        sender.isOpaque = false
+    }
+    
+    func playSound(_ soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
