78d2db09f8e7a801b5a977eab7419689f7c174b6
Hugo Andreassa <hugo.andreassa@gmail.com>
2021-08-31 16:46:07 -0300

    egg timer

--- 5 - egg-timer/EggTimer/ViewController.swift
+++ 5 - egg-timer/EggTimer/ViewController.swift
@@ -10,6 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var timerProgressView: UIProgressView!
+    
+    var eggTime = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var timer: Timer?
+    var totalTime: Float = 0.0
+    var secondsPassed: Float = 0.0
+    var secondsRemaining = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        
+        if let seconds = eggTime[hardness] {
+            
+            timer?.invalidate()
+            
+            secondsRemaining = seconds
+            totalTime = Float(seconds)
+            
+            timer = Timer.scheduledTimer(
+                timeInterval: 1.0,
+                target: self,
+                selector: #selector(updateTimer),
+                userInfo: nil,
+                repeats: true)
+        }
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            timerProgressView.progress = secondsPassed / totalTime
+            secondsPassed += 1
+        }
+        
+        if secondsRemaining >= 0 {
+            timerLabel.text = "\(String(secondsRemaining)) seconds."
+            secondsRemaining -= 1
+        } else {
+            timer?.invalidate()
+        }
+    }
 }
