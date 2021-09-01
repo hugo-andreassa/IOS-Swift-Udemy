146de3b8038b4bd7b96b0bf49239d463c7611fb6
Hugo Andreassa <hugo.andreassa@gmail.com>
2021-09-01 14:22:10 -0300

    Commit

---
+++ 6 - quizzler/Quizzler-iOS13/model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Virtual Machine on 01/09/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
---
+++ 8 - bmi-calculator/BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Virtual Machine on 01/09/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
