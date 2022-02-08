0eea3a2f8852e3c9d82ddec502e7d58830b98be2
Hugo Andreassa <hugo.andreassa@gmail.com>
2022-02-08 09:45:18 -0300

    CoreData setup

---
+++ 14 - coreDataTest/CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Hugo Andreassa Amaral (P) on 08/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
